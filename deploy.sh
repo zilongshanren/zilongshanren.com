fd9cc1912e0aeaa32692dc5a942add2d042a6481
zilongshanren <guanghui8827@126.com>
2022-08-21 22:00:44 +0800

    add new blog posts

--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,5 @@
-hugo --theme=even --baseUrl="https://zilongshanren.com"
-hugo && rsync -avz --delete public/ root@txyun:/var/www/octopress-cn
+# hugo --theme=even --baseUrl="https://zilongshanren.com"
+# hugo && rsync -avz --delete public/ root@txyun:/var/www/octopress-cn
+cp docs/CNAME public
+rm -rf docs
+cp -r public docs
